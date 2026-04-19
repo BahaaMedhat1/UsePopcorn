7b62323635f8166076da4ed9ccc010c850cf7e1c
BahaaMedhat1 <bahaawanas427@gmail.com>
2026-04-19 21:30:49 +0200

    Refactor index.js to use App component
    Updated imports and removed unused components.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import App from "./App-v1";
-import StarRating from "./StarRating";
-// import { useState } from "react";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const Test = function () {
-  const [movieRating, setMovieRating] = React.useState(0);
-  return (
-    <div>
-      <StarRating onSetRating={setMovieRating} color="blue" />
-      <p>The movie Rating {movieRating}</p>
-    </div>
-  );
-};
 root.render(
   <React.StrictMode>
-    {/* <StarRating />
-    <StarRating
-      maxRating={10}
-      size={"bbbb"}
-      color="red"
-      messages={["Terrible", "Medium", "Okay", "Good", "Amazing"]}
-      defaultRating={10}
-    />
-    <Test /> */}
+  
     <App />
   </React.StrictMode>,
 );
